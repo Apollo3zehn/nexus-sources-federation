7e2466952dca22979451a786d077bd8442ebab78
Apollo3zehn <git@m1.apollo3zehn.net>
2022-10-04 17:50:44 +0200

    Fixes.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v1.0.0-beta.2 - 2022-10-04
+
+Some improvements.
+
 ## v1.0.0-beta.1 - 2022-10-04
 
 This is a preparing release.
--- src/Nexus.Sources.Federation/Federation.cs
+++ src/Nexus.Sources.Federation/Federation.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http.Headers;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.Extensions.Logging;
 using Nexus.DataModel;
 using Nexus.Extensibility;
@@ -14,7 +15,15 @@ namespace Nexus.Sources
     {
         private DataSourceContext _context = default!;
         private Api.NexusClient _nexusClient = default!;
-        private string _rootCatalogId = default!;
+        private string _mountPath = default!;
+
+        private static JsonSerializerOptions _jsonSerializerOptions;
+
+        static Federation()
+        {
+            _jsonSerializerOptions = new JsonSerializerOptions();
+            _jsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        }
 
         public Task SetContextAsync(DataSourceContext context, ILogger logger, CancellationToken cancellationToken)
         {
@@ -30,65 +39,100 @@ namespace Nexus.Sources
             };
 
             // token
-            var token = _context.SourceConfiguration.GetStringValue($"bearer-token");
+            var token = _context.SourceConfiguration.GetStringValue($"access-token");
 
             if (token is null)
-                throw new Exception("The bearer-token property is not set.");
+                throw new Exception("The access-token property is not set.");
 
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
 
             _nexusClient = new Api.NexusClient(httpClient);
 
-            // root-catalog-id
-            var rootCatalogId = _context.SourceConfiguration.GetStringValue($"root-catalog-id");
+            // mount-path
+            var mountPath = _context.SourceConfiguration.GetStringValue($"mount-path");
 
-            if (rootCatalogId is null)
-                throw new Exception("The root-catalog-id property is not set.");
+            if (mountPath is null)
+                throw new Exception("The mount-path property is not set.");
 
-            _rootCatalogId = rootCatalogId;
+            _mountPath = mountPath;
 
             return Task.CompletedTask;
         }
 
         public async Task<CatalogRegistration[]> GetCatalogRegistrationsAsync(string path, CancellationToken cancellationToken)
         {
-            var catalogInfos = await _nexusClient.Catalogs.GetChildCatalogInfosAsync("", cancellationToken);
+            if (path == "/")
+            {
+                return new CatalogRegistration[] { new CatalogRegistration(_mountPath, default) };
+            }
 
-            return catalogInfos
-                .Select(catalogInfo => new CatalogRegistration(GetRealCatalogId(catalogInfo.Id), catalogInfo.Title, IsTransient: true))
-                .ToArray();
+            else
+            {
+                var catalogInfos = await _nexusClient.Catalogs.GetChildCatalogInfosAsync(GetOriginalCatalogId(path), cancellationToken);
+
+                return catalogInfos
+                    .Where(catalogInfo => catalogInfo.Id != _mountPath)
+                    .Select(catalogInfo => new CatalogRegistration(GetExtendedCatalogId(catalogInfo.Id), catalogInfo.Title, IsTransient: true))
+                    .ToArray();
+            }
         }
 
         public async Task<ResourceCatalog> GetCatalogAsync(string catalogId, CancellationToken cancellationToken)
         {
-            var resourceCatalog = await _nexusClient.Catalogs.GetAsync(GetRealCatalogId(catalogId));
-            var jsonString = JsonSerializer.Serialize(resourceCatalog);
-            var actualResourceCatalog = JsonSerializer.Deserialize<ResourceCatalog>(jsonString)!;
+            if (catalogId == _mountPath)
+            {
+                return new ResourceCatalog(catalogId);
+            }
 
-            return actualResourceCatalog;
+            else
+            {
+                var resourceCatalog = await _nexusClient.Catalogs.GetAsync(GetOriginalCatalogId(catalogId));
+                resourceCatalog = resourceCatalog with { Id = catalogId };
+
+                var jsonString = JsonSerializer.Serialize(resourceCatalog, _jsonSerializerOptions);
+                var actualResourceCatalog = JsonSerializer.Deserialize<ResourceCatalog>(jsonString, _jsonSerializerOptions)!;
+
+                return actualResourceCatalog;
+            }
         }
 
         public async Task<double> GetAvailabilityAsync(string catalogId, DateTime begin, DateTime end, CancellationToken cancellationToken)
         {
-            var availability = await _nexusClient.Catalogs
-                .GetAvailabilityAsync(GetRealCatalogId(catalogId), begin, end, (end - begin), cancellationToken);
+            if (catalogId == _mountPath)
+            {
+                return 1.0;
+            }
 
-            return availability.Data[0];
+            else
+            {
+                var availability = await _nexusClient.Catalogs
+                    .GetAvailabilityAsync(GetOriginalCatalogId(catalogId), begin, end, (end - begin), cancellationToken);
+
+                return availability.Data[0];
+            }
         }
 
         public async Task<(DateTime Begin, DateTime End)> GetTimeRangeAsync(string catalogId, CancellationToken cancellationToken)
         {
-            var timeRange = await _nexusClient.Catalogs
-                .GetTimeRangeAsync(GetRealCatalogId(catalogId), cancellationToken);
+            if (catalogId == _mountPath)
+            {
+                return (DateTime.MinValue, DateTime.MaxValue);
+            }
 
-            return (timeRange.Begin, timeRange.End);
+            else
+            {
+                var timeRange = await _nexusClient.Catalogs
+                    .GetTimeRangeAsync(GetOriginalCatalogId(catalogId), cancellationToken);
+
+                return (timeRange.Begin, timeRange.End);
         }
+            }
 
         public async Task ReadAsync(DateTime begin, DateTime end, ReadRequest[] requests, ReadDataHandler readData, IProgress<double> progress, CancellationToken cancellationToken)
         {
             foreach (var request in requests)
             {
-                var streamResponse = await _nexusClient.Data.GetStreamAsync(request.CatalogItem.ToPath(), begin, end, cancellationToken);
+                var streamResponse = await _nexusClient.Data.GetStreamAsync(GetOriginalCatalogId(request.CatalogItem.ToPath()), begin, end, cancellationToken);
                 var stream = await streamResponse.GetStreamAsync();
                 var targetBuffer = request.Data;
 
@@ -102,9 +146,14 @@ namespace Nexus.Sources
             }
         }
 
-        private string GetRealCatalogId(string catalogId)
+        private string GetExtendedCatalogId(string catalogId)
         {
-            return _rootCatalogId + catalogId;
+            return _mountPath + catalogId;
+        }
+
+        private string GetOriginalCatalogId(string catalogId)
+        {
+            return catalogId.Substring(_mountPath.Length);
         }
     }
 }
